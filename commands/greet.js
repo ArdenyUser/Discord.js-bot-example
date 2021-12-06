d9366742070c9d2cd1e0dba46255b1b6b377652a
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-06 08:07:22 -0500

    Update greet.js

--- commands/greet.js
+++ commands/greet.js
@@ -1,1 +1,5 @@
-
+exports.run = (client, message, args) => {
+    const sayMessage = "Hello, comrades! Servant is here to help..."
+    message.delete().catch(O_o=>{}); // Ignores the error 
+    message.channel.send(sayMessage);
+  }
